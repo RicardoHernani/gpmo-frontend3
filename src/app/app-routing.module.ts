e54b3894cb4aa50ad654db32a68483cb5d2faaf0
RicardoHernani <ricardohernani@yahoo.com.br>
2022-07-17 00:03:44 -0300

    Navegacao entre as paginas login e referencias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'referencias',
+    loadChildren: () => import('./pages/referencias/referencias.module').then( m => m.ReferenciasPageModule)
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public menuCollection = [
     { title: 'Home', url: 'home', icon: 'home' },
+    { title: 'Referência', url: 'referencias', icon: 'reader' },
   ];
 
   constructor() {}
