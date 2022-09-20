3cf81676620583247a40c2fa2a59ea5271f9080d
RicardoHernani <ricardohernani@yahoo.com.br>
2022-09-20 18:25:40 -0300

    Criacao da pagina de entrada de dados de relatorio

--- src/app/pages/relatorios-producao/relatorios-producao.page.ts
+++ src/app/pages/relatorios-producao/relatorios-producao.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-relatorios-producao',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RelatoriosProducaoPage implements OnInit {
 
-  constructor() { }
+  inserirDadosFormGroup: FormGroup= this.formBuilder.group({
+    dataInicial: ['', [Validators.required]],
+    dataFinal: ['', [Validators.required]],
+    diasValidos: ['', [Validators.required, Validators.min(0), Validators.max(25)]],
+    pontosExtras: [64, [Validators.required, Validators.min(64), Validators.max(400)]],
+    });
+
+  constructor(
+    public formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
+
+  }
+
+  inserirDados() {
+
   }
 
 }
