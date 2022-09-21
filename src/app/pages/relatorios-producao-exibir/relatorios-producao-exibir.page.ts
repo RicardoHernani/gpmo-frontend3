9ed067a683185a8af574fd5acfb2a8daf2533499
RicardoHernani <ricardohernani@yahoo.com.br>
2022-09-21 17:00:08 -0300

    Calculos e apresentacao console ao entrar em relatorios

--- src/app/pages/relatorios-producao-exibir/relatorios-producao-exibir.page.ts
+++ src/app/pages/relatorios-producao-exibir/relatorios-producao-exibir.page.ts
@@ -1,6 +1,5 @@
 import { CirurgiaDTO } from 'src/app/models/cirurgia.dto';
 import { CirurgiaService } from 'src/app/services/domain/cirurgia.service';
-/* eslint-disable radix */
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
@@ -13,29 +12,44 @@ export class RelatoriosProducaoExibirPage implements OnInit {
 
   dataInicial: string = this.route.snapshot.queryParamMap.get('dataInicial');
   dataFinal: string = this.route.snapshot.queryParamMap.get('dataFinal');
-  diasValidos: number = parseInt((this.route.snapshot.queryParamMap.get('diasValidos')));
-  pontosExtras: number = parseInt((this.route.snapshot.queryParamMap.get('pontosExtras')));
+  diasValidos: number = parseInt((this.route.snapshot.queryParamMap.get('diasValidos')), 10);
+  pontosExtras: number = parseInt((this.route.snapshot.queryParamMap.get('pontosExtras')), 10);
 
   cirurgias: CirurgiaDTO[];
+  totalPontos = 0;
+  totalValor = 0;
 
   constructor(
     public route: ActivatedRoute,
     public cirurgiaService: CirurgiaService) {
-     }
-
-  ngOnInit() {
-
   }
 
-  mostrarPorIntervaloData(){
+  ngOnInit() {
     this.cirurgiaService.findCirurgiasByDateInterval(this.dataInicial, this.dataFinal)
       .subscribe(resposta => {
         // eslint-disable-next-line @typescript-eslint/dot-notation
         this.cirurgias = (resposta['content']);
+        console.log(this.cirurgias);
+
       },
         error => {
         });
   }
 
+  ionViewDidEnter() {
+    const pontosNecessarios = (this.diasValidos * 16) - this.pontosExtras;
+    this.calcularTotais();
+    console.log(pontosNecessarios);
+    console.log(this.totalPontos, this.totalValor);
+  }
+
+
+  calcularTotais() {
+    // eslint-disable-next-line @typescript-eslint/prefer-for-of
+    for (let i = 0; i<this.cirurgias.length; i++) {
+      this.totalPontos = this.cirurgias[i].subTotalPontos + this.totalPontos;
+      this.totalValor = this.cirurgias[i].subTotalValor + this.totalValor;
+    }
+  }
 
 }
